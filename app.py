d070ebfc6c48a2bec4b7d828df85c08140da1387
Shelbyy <ncp9988@gmail.com>
2023-12-22 12:43:35 -0600

    add message.html and requirements.txt file

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from flask import Flask,render_template, request
 app = Flask(__name__)
 
 
-@app.route('/submit', methods=['GET']) 
+@app.route('/', methods=['GET']) 
 def show_form():
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit_form():
     data = request.form.get('message') #accessing form data with the name 'messgae'
-    # Process or store 'data' (save to database, send an email)
     print(f"Received message: {data}")
-    return "from submmited successfully!"
+    return render_template('message.html')
 
 if __name__ == '__main__':
     app.run(debug=True) # run the Flask app
